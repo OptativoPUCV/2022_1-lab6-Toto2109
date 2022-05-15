6d65697d4be15145ac5c5acace6c72cd2dc11cda
Your Name <you@example.com>
2022-05-15 17:41:04 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -50,10 +50,10 @@ int is_valid(Node* n){
 
 
 List* get_adj_nodes(Node* n){
-    List* list=createList();
-    for(int i = 0; i < 9; i++){
-      for(int j = 0; j < 9 ; j++){
-        if(n -> sudo[i][j] == 0);
+  List* list=createList();
+  for(int i = 0; i < 9; i++){
+    for(int j = 0; j < 9 ; j++){
+      if(n -> sudo[i][j] == 0){
         for(int k = 1; k < 10; k++ ){
           n -> sudo [i][j] = k;
           if(is_valid(n)){
@@ -64,7 +64,8 @@ List* get_adj_nodes(Node* n){
         return list;
       }
     }
-    return list;
+  }
+  return list;
 }
 
 
