eb4c1b766456affb966daab9229cf55bd7adc007
Your Name <you@example.com>
2022-05-15 17:35:54 -0400

    new try

--- sudoku.c
+++ sudoku.c
@@ -54,7 +54,7 @@ List* get_adj_nodes(Node* n){
     for(int i = 0; i < 9; i++){
       for(int j = 0; j < 9 ; j++){
         if(n -> sudo[i][j] == 0);
-        for(int k = 0; k < 10; k++ ){
+        for(int k = 1; k < 10; k++ ){
           n -> sudo [i][j] = k;
           if(is_valid(n)){
             Node *new = copy(n);
